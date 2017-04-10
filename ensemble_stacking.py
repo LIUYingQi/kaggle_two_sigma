f997d009d63176dafe5e2691657f80ef5fcc8a0d
liuyingqi <billliuyingqi@126.com>
2017-04-10 14:58:02 +0800

    hahah

--- ensemble_stacking.py
+++ ensemble_stacking.py
@@ -5,62 +5,43 @@ from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
-
+from sklearn.preprocessing import StandardScaler
 
 
 
 if __name__ == '__main__':
-
-    np.random.seed(0)  # seed to shuffle the train set
-
-    n_folds = 10
-    verbose = True
-    shuffle = False
+    # train = np.loadtxt('stack_net/train_stacknet.csv',delimiter=',')
+    # print train.shape
+    # test = np.loadtxt('stack_net/test_stacknet.csv',delimiter=',')
+    # print test.shape
 
 
-    if shuffle:
-        idx = np.random.permutation(y.size)
-        X = X[idx]
-        y = y[idx]
+    f = open('pkl_files/train_df_2.pkl','rb')
+    trainset = pickle.load(f)
+    f.close()
+    f = open('pkl_files/test_df_2.pkl','rb')
+    testset = pickle.load(f)
+    f.close()
+    f = open('pkl_files/label_2.pkl','rb')
+    label = pickle.load(f)
+    f.close()
 
-    skf = list(StratifiedKFold(y, n_folds))
+    print trainset.shape
+    print testset.shape
 
-    clfs = [RandomForestClassifier(n_estimators=100, n_jobs=-1, criterion='gini'),
-            RandomForestClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
-            ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='gini'),
-            ExtraTreesClassifier(n_estimators=100, n_jobs=-1, criterion='entropy'),
-            GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=50)]
+    label = np.reshape(label,(label.size,1))
+    testset_listing_id = testset['listing_id'].values
+    testset_listing_id = np.reshape(testset_listing_id,(testset_listing_id.size,1))
+    trainset = trainset.values
+    testset = testset.values
+    stander = StandardScaler()
+    total_set = np.vstack((trainset,testset))
+    stander.fit(total_set)
+    trainset = stander.transform(trainset)
+    testset = stander.transform(testset)
 
-    print "Creating train and test sets for blending."
+    trainset = np.hstack((label,trainset))
+    testset = np.hstack((testset_listing_id,testset))
 
-    dataset_blend_train = np.zeros((X.shape[0], len(clfs)))
-    dataset_blend_test = np.zeros((X_submission.shape[0], len(clfs)))
-
-    for j, clf in enumerate(clfs):
-        print j, clf
-        dataset_blend_test_j = np.zeros((X_submission.shape[0], len(skf)))
-        for i, (train, test) in enumerate(skf):
-            print "Fold", i
-            X_train = X[train]
-            y_train = y[train]
-            X_test = X[test]
-            y_test = y[test]
-            clf.fit(X_train, y_train)
-            y_submission = clf.predict_proba(X_test)[:, 1]
-            dataset_blend_train[test, j] = y_submission
-            dataset_blend_test_j[:, i] = clf.predict_proba(X_submission)[:, 1]
-        dataset_blend_test[:, j] = dataset_blend_test_j.mean(1)
-
-    print
-    print "Blending."
-    clf = LogisticRegression()
-    clf.fit(dataset_blend_train, y)
-    y_submission = clf.predict_proba(dataset_blend_test)[:, 1]
-
-    print "Linear stretch of predictions to [0,1]"
-    y_submission = (y_submission - y_submission.min()) / (y_submission.max() - y_submission.min())
-
-    print "Saving Results."
-    tmp = np.vstack([range(1, len(y_submission)+1), y_submission]).T
-    np.savetxt(fname='submission.csv', X=tmp, fmt='%d,%0.9f',
-               header='MoleculeId,PredictedProbability', comments='')
+    np.savetxt('xgboost_2_train_stacknet.csv',trainset,delimiter=',',fmt='%.5f')
+    np.savetxt('xgboost_2_test_stacknet.csv',testset,delimiter=',',fmt='%.5f')
